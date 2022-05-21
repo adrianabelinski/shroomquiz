898e407b02618a3880d83ccdd7cc1affddc45e8a
Adriana Belinski <adrianab@protonmail.com>
2022-05-21 17:41:31 -0500

    Renamed ContentView to QuizView, fixed indentation, added mushroom photo.

---
+++ ShroomQuiz/QuizView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct QuizView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct QuizView_Previews: PreviewProvider {
+  static var previews: some View {
+    QuizView()
+  }
+}
--- ShroomQuiz/ShroomQuizApp.swift
+++ ShroomQuiz/ShroomQuizApp.swift
@@ -1,17 +1,10 @@
-//
-//  ShroomQuizApp.swift
-//  ShroomQuiz
-//
-//  Created by Adriana Belinski on 5/21/22.
-//
-
 import SwiftUI
 
 @main
 struct ShroomQuizApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      QuizView()
     }
+  }
 }
