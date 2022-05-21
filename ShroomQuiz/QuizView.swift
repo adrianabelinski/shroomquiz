5a129d5b0ab6aaa743cc29fd87f4a127158aa2b1
Adriana Belinski <adrianab@protonmail.com>
2022-05-21 17:51:25 -0500

    Added mushroom quiz buttons.

--- ShroomQuiz/QuizView.swift
+++ ShroomQuiz/QuizView.swift
@@ -2,13 +2,29 @@ import SwiftUI
 
 struct QuizView: View {
   var body: some View {
-    Image("flyagaric")
-      .resizable()
-      .scaledToFit()
-      .cornerRadius(10)
-      .padding()
+    VStack {
+      Image("flyagaric")
+        .resizable()
+        .scaledToFit()
+        .cornerRadius(10)
+        .padding()
+      Button(action: {}) {
+        Text("Hericium erinaceus")
+      }
+      Button(action: {}) {
+        Text("Amanita muscaria")
+      }
+      Button(action: {}) {
+        Text("Pleurotus")
+      }
+      Button(action: {}) {
+        Text("Morchella")
+      }
+    }
   }
 }
+
+
 
 struct QuizView_Previews: PreviewProvider {
   static var previews: some View {
