a3138a9c892f3ac2320a125194eceeab445b15ce
Adriana Belinski <adrianab@protonmail.com>
2022-05-21 17:29:03 -0500

    Initial Commit

---
+++ ShroomQuiz/ShroomQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShroomQuizApp.swift
+//  ShroomQuiz
+//
+//  Created by Adriana Belinski on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ShroomQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
