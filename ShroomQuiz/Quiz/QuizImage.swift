873559d82a453098103b459e3c4a63d78dea2405
Adriana Belinski <adrianab@protonmail.com>
2022-06-19 18:27:11 -0500

    Extracted overlay text view into a separate variable.

--- ShroomQuiz/Quiz/QuizImage.swift
+++ ShroomQuiz/Quiz/QuizImage.swift
@@ -13,26 +13,32 @@ struct QuizImage: View {
       .resizable()
       .aspectRatio(contentMode: .fit)
       .overlay {
-        if let overlayText = overlayText, let overlayMessageType = overlayMessageType {
-          VStack {
-            Spacer()
-            HStack {
-              Spacer()
-              Text(overlayText)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-                .padding()
-              Spacer()
-            }
-            .background(
-              (overlayMessageType == .correct)
-                ? Color.gemGreen.opacity(0.8)
-                : Color.yellow.opacity(0.8)
-            )
-          }
-        }
+        overlayTextView
       }
       .cornerRadius(10)
       .padding()
+  }
+  
+  var overlayTextView: some View {
+    Group {
+      if let overlayText = overlayText, let overlayMessageType = overlayMessageType {
+        VStack {
+          Spacer()
+          HStack {
+            Spacer()
+            Text(overlayText)
+              .fontWeight(.bold)
+              .foregroundColor(.white)
+              .padding()
+            Spacer()
+          }
+          .background(
+            (overlayMessageType == .correct)
+            ? Color.gemGreen.opacity(0.8)
+            : Color.yellow.opacity(0.8)
+          )
+        }
+      }
+    }
   }
 }
