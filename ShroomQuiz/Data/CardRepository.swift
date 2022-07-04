ecd7d2df5683fce72fa6281557caeb5a9cf61176
Adriana Belinski <adrianab@protonmail.com>
2022-07-04 18:48:00 -0500

    Indentation.

--- ShroomQuiz/Data/CardRepository.swift
+++ ShroomQuiz/Data/CardRepository.swift
@@ -2,22 +2,86 @@ import UIKit
 
 struct CardRepository {
   let cards: Set<Card> = [ //Set has only unique items and order doesn't matter
-    Card(scientificName: "Amanita muscaria", commonName: "Fly Agaric", imageName: "fly-agaric"),
-    Card(scientificName: "Pleurotus ostreatus", commonName: "Oyster Mushroom", imageName: "oyster-mushroom"),
-    Card(scientificName: "Morchella", commonName: "Morel Mushroom", imageName: "morel-mushroom"),
-    Card(scientificName: "Laetiporus", commonName: "Chicken of the Woods", imageName: "chicken-of-the-woods"),
-    Card(scientificName: "Cantharellus", commonName: "Chanterelle", imageName: "chanterelle"),
-    Card(scientificName: "Trametes versicolor", commonName: "Turkey Tail", imageName: "turkey-tail"),
-    Card(scientificName: "Ramaria stricta", commonName: "Coral Fungus", imageName: "coral-fungus"),
-    Card(scientificName: "Auricularia auricula-judae", commonName: "Woods Ear", imageName: "woods-ear"),
-    Card(scientificName: "Astraeus hygrometricus", commonName: "Earthstar Mushroom", imageName: "earth-star"),
-    Card(scientificName: "Coprinopsis atramentaria", commonName: "Inky Cap", imageName: "inky-cap"),
-    Card(scientificName: "Boletus edulis", commonName: "Porcini", imageName: "porcini"),
-    Card(scientificName: "Cerioporus squamosus", commonName: "Pheasant Back", imageName: "pheasant-back"),
-    Card(scientificName: "Phallus indusiatus", commonName: "Bridal Veil Stinkhorn", imageName: "bridal-veil-stinkhorn"),
-    Card(scientificName: "Flammulina velutipes", commonName: "Enoki", imageName: "enoki"),
-    Card(scientificName: "Lycoperdon perlatum", commonName: "Common Puffball", imageName: "common-puffball"),
-    Card(scientificName: "Armillaria mellea", commonName: "Honey Fungus", imageName: "honey-fungus")
+    Card(
+      scientificName: "Amanita muscaria",
+      commonName: "Fly Agaric",
+      imageName: "fly-agaric"
+    ),
+    Card(
+      scientificName: "Pleurotus ostreatus",
+      commonName: "Oyster Mushroom",
+      imageName: "oyster-mushroom"
+    ),
+    Card(
+      scientificName: "Morchella",
+      commonName: "Morel Mushroom",
+      imageName: "morel-mushroom"
+    ),
+    Card(
+      scientificName: "Laetiporus",
+      commonName: "Chicken of the Woods",
+      imageName: "chicken-of-the-woods"
+    ),
+    Card(
+      scientificName: "Cantharellus",
+      commonName: "Chanterelle",
+      imageName: "chanterelle"
+    ),
+    Card(
+      scientificName: "Trametes versicolor",
+      commonName: "Turkey Tail",
+      imageName: "turkey-tail"
+    ),
+    Card(
+      scientificName: "Ramaria stricta",
+      commonName: "Coral Fungus",
+      imageName: "coral-fungus"
+    ),
+    Card(
+      scientificName: "Auricularia auricula-judae",
+      commonName: "Woods Ear",
+      imageName: "woods-ear"
+    ),
+    Card(
+      scientificName: "Astraeus hygrometricus",
+      commonName: "Earthstar Mushroom",
+      imageName: "earth-star"
+    ),
+    Card(
+      scientificName: "Coprinopsis atramentaria",
+      commonName: "Inky Cap",
+      imageName: "inky-cap"
+    ),
+    Card(
+      scientificName: "Boletus edulis",
+      commonName: "Porcini",
+      imageName: "porcini"
+    ),
+    Card(
+      scientificName: "Cerioporus squamosus",
+      commonName: "Pheasant Back",
+      imageName: "pheasant-back"
+    ),
+    Card(
+      scientificName: "Phallus indusiatus",
+      commonName: "Bridal Veil Stinkhorn",
+      imageName: "bridal-veil-stinkhorn"
+    ),
+    Card(
+      scientificName: "Flammulina velutipes",
+      commonName: "Enoki",
+      imageName: "enoki"
+    ),
+    Card(
+      scientificName: "Lycoperdon perlatum",
+      commonName: "Common Puffball",
+      imageName: "common-puffball"
+    ),
+    Card(
+      scientificName: "Armillaria mellea",
+      commonName: "Honey Fungus",
+      imageName: "honey-fungus"
+    )
   ]
   
   init() { 
