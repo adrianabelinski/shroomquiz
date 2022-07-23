d87fe5b903006c85aee529985c565aa8a1cbf053
Adriana Belinski <adrianab@protonmail.com>
2022-07-23 11:11:05 -0500

    Organized code.

--- ShroomQuiz/UI/Detail/DetailViewModel.swift
+++ ShroomQuiz/UI/Detail/DetailViewModel.swift
@@ -3,11 +3,8 @@ import UIKit
 
 
 struct DetailViewModel {
-  private let favoritesProvider = FavoritesProvider()
-
-  var isFavorited: Bool {
-    favoritesProvider.isFavorited(card: card)
-  }
+  
+  // MARK: - Public properties
   
   var imageName: String { //Type notation
     card.imageName
@@ -21,15 +18,27 @@ struct DetailViewModel {
     card.scientificName
   }
   
+  var isFavorited: Bool {
+    favoritesProvider.isFavorited(card: card)
+  }
+  
   var description: String {
     card.description
   }
   
+  // MARK: - Private properties
+  
   private let card: Card
+  
+  private let favoritesProvider = FavoritesProvider()
+  
+  // MARK: - Init
   
   init(card: Card) {
     self.card = card
   }
+  
+  // MARK: - Public methods
   
   func openWikipedia() {
     if let wikipediaUrl = URL(string: card.wikipediaUrlString) {
