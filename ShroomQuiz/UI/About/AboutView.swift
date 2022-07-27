4e12e25005e7bc0f7112ea01658eea936f060b59
Adriana Belinski <adrianab@protonmail.com>
2022-07-26 19:24:58 -0500

    Added BodyText, added more text to About page.

--- ShroomQuiz/UI/About/AboutView.swift
+++ ShroomQuiz/UI/About/AboutView.swift
@@ -2,8 +2,20 @@ import SwiftUI
 
 struct AboutView: View {
   var body: some View {
-    Text("Shroom Quiz is made by Adriana Belinski.")
-      .navigationTitle("About")
+    ScrollView {
+      VStack(alignment: .leading, spacing: 10) {
+        BodyText("ShroomQuiz is a mushroom guessing game for mycelium-loving maniacs of all kinds. Study your favorites mushrooms, and learn more about their characteristics, edibility, and where they grow.")
+          
+        Text("Disclaimer")
+          .font(.title)
+          .bold()
+          .foregroundColor(Color.rubyRed)
+        BodyText("Use this information at your own risk. The information here is used to teach and entertain, but is by no means reviewed by fungi experts. Please don't go picking and chewing on every mushroom your find, because that'd be bad! Consult the pros before you begin any actual foraging.")
+        
+      }
+      .padding()
+    }
+    .navigationTitle("About")
   }
 }
 
