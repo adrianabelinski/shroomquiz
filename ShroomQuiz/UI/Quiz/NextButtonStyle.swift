9315a91ea9ddbd22eba9176fbc40aa87f090491f
Adriana Belinski <adrianab@protonmail.com>
2022-07-28 21:37:10 -0500

    Updated quiz button styles.

--- ShroomQuiz/UI/Quiz/NextButtonStyle.swift
+++ ShroomQuiz/UI/Quiz/NextButtonStyle.swift
@@ -3,10 +3,11 @@ import SwiftUI
 struct NextButtonStyle: ButtonStyle {
   func makeBody(configuration: Self.Configuration) -> some View {
     return configuration.label
-      .font(.title2)
+      .font(.title.bold())
+      .shadow(color: .black.opacity(0.3), radius: 2, x: 1, y: 1)
       .frame(width: 300, height: 50, alignment: .center)
       .background(Color.orange)
-      .foregroundColor(Color.black)
+      .foregroundColor(Color.white)
       .cornerRadius(10)
       .padding(7)
   }
@@ -16,9 +17,9 @@ struct NextButtonStyle: ButtonStyle {
 struct NextButtonStyle_Previews: PreviewProvider {
   static var previews: some View {
     Button(action: {}) {
-      Text("Button with QuizButtonStyle")
+      Text("Button")
     }
-    .buttonStyle(QuizButtonStyle())
+    .buttonStyle(NextButtonStyle())
     .previewLayout(.sizeThatFits)
   }
 }
--- ShroomQuiz/UI/Quiz/QuizButtonStyle.swift
+++ ShroomQuiz/UI/Quiz/QuizButtonStyle.swift
@@ -3,10 +3,11 @@ import SwiftUI
 struct QuizButtonStyle: ButtonStyle {
   func makeBody(configuration: Self.Configuration) -> some View {
     return configuration.label
-      .font(.title2)
+      .font(.title2.bold())
+      .shadow(color: .black.opacity(0.3), radius: 2, x: 1, y: 1)
       .frame(width: 300, height: 50, alignment: .center)
       .background(Color.gemGreen)
-      .foregroundColor(Color.black)
+      .foregroundColor(Color.white)
       .cornerRadius(10)
       .padding(7)
   }
@@ -15,7 +16,7 @@ struct QuizButtonStyle: ButtonStyle {
 struct QuizButtonStyle_Previews: PreviewProvider {
   static var previews: some View {
     Button(action: {}) {
-      Text("Button with QuizButtonStyle")
+      Text("Button")
     }
     .buttonStyle(QuizButtonStyle())
     .previewLayout(.sizeThatFits)
