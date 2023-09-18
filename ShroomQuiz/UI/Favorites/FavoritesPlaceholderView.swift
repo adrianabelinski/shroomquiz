219378ad78bbe2b274c862b2c3758f86c89f7140
Adriana Belinski <adrianabmpls@gmail.com>
2023-09-18 13:31:51 -0500

    Added basket image to favorites screen.

--- ShroomQuiz/UI/Favorites/FavoritesPlaceholderView.swift
+++ ShroomQuiz/UI/Favorites/FavoritesPlaceholderView.swift
@@ -1,13 +1,19 @@
 import SwiftUI
 
 struct FavoritesPlaceholderView: View {
-    var body: some View {
-        Text("When you press the star button on a card, it will show up here.")
+  var body: some View {
+    VStack {
+      Image("favorites-bucket")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .ignoresSafeArea()
+      Text("When you press the star button on a card, it will show up here.")
     }
+  }
 }
 
 struct FavoritesPlaceholderView_Previews: PreviewProvider {
-    static var previews: some View {
-      FavoritesPlaceholderView()
-    }
+  static var previews: some View {
+    FavoritesPlaceholderView()
+  }
 }
